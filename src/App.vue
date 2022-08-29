d25b243d81666a9e4aff2712e7093aa7ff8482af
ngovantrinh <trinhtamtao@gmail.com>
2022-08-29 11:22:41 +0700

    update example

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <HelloWorld @getLocationByText="getLocation($event)" />
+  </div>
+</template>
+
+<script>
+// import axios from 'axios';
+// import  from './constants/constant'
+import HelloWorld from './components/HelloWorld.vue';
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+};
+</script>
---
+++ src/components/SelectOption.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="list-selected">
+    <div class="select__item">
+      <span>{{ item }}</span>
+      <img
+        src="../assets/images/X.png"
+        class="remove-item"
+        @click="handleItemRemove(item)"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+// console.log(item);
+export default {
+  name: 'SelectOption',
+  props: {
+    item: {
+      type: [String, Number, Array, Object],
+    },
+  },
+  methods: {
+    handleItemRemove(item) {
+      this.$emit('handleItemRemove', { item });
+    },
+  },
+};
+</script>
+
+<style scoped>
+.remove-item {
+  cursor: pointer;
+  margin-left: 5px;
+}
+
+.list-selected {
+  display: flex;
+  align-items: center;
+}
+
+.select__item {
+  background: #f0f4f8;
+  border: 1px solid #dcdcdc;
+  border-radius: 4px;
+  padding: 4px 8px;
+}
+</style>
